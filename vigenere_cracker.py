1149cc849c4825848a8c631757fe821cf5e151b4
zancas <zancas@leastauthority.com>
2014-11-16 19:50:47 -0700

    Different between key length and key inference

--- vigenere_cracker.py
+++ vigenere_cracker.py
@@ -7,20 +7,19 @@ def collect_modular_elements(vigenere_cyphertext):
     max_len = len(vigenere_cyphertext)
     for stream_length in xrange(1, max_len):
         print "stream length: %s" % stream_length
-        modulus_streams[stream_length] = create_Nth_stream(vigenere_cyphertext, stream_length, max_len)
+        modulus_streams[stream_length] = create_Nth_stream(vigenere_cyphertext, stream_length)
+        print
 
     return modulus_streams
 
-def create_Nth_stream(iterable, N, max_len):
+def create_Nth_stream(iterable, N):
     Nth_stream = {}
     for index, element in enumerate(iterable):
         try:
             Nth_stream[index%N].append(element)
         except KeyError:
             Nth_stream[index%N] = [element]
-    print "Nth_stream: %s" % Nth_stream
-    scaling_factor = float(max_len) / float(N)
-    find_scaled_most_freq_in_Nth_stream(Nth_stream, scaling_factor)
+    find_scaled_most_freq_in_Nth_stream(Nth_stream, N)
     return Nth_stream
 
 def most_freq_in_list(iterable):
@@ -35,20 +34,24 @@ def most_freq_in_list(iterable):
     return most_frequent
 
 def find_scaled_most_freq_in_Nth_stream(Nth_stream, scaling_factor):
-    max_seen = 0
+    count_of_most_frequent = 0
     for obs_vals in Nth_stream.values():
         max_count = most_freq_in_list(obs_vals)
-        scaled_max = max_count / scaling_factor
-        if max_seen < scaled_max:
-            max_seen = scaled_max
-    print "max_seen is %s" % max_seen
-    return max_seen
+        #print max_count
+        scaled_max = max_count * scaling_factor
+        if count_of_most_frequent < scaled_max:
+            count_of_most_frequent = scaled_max
+    print "count_of_most_frequent is %s" % count_of_most_frequent
+    return count_of_most_frequent
 
 def main():
     import sys
-    fc = (open(sys.argv[1], 'r').read()).strip()
-    chunked_cyphertext = [x for x in chunker(fc, 2)]
-    collect_modular_elements(chunked_cyphertext)
+    if sys.argv[1] == 'read':
+        fc = (open(sys.argv[2], 'r').read()).strip()
+        chunked_cyphertext = [x for x in chunker(fc, 2)]
+        collect_modular_elements(chunked_cyphertext)
+
+        #stream_7 = create_Nth_stream(chunked_cyphertext, 7)
 
 if __name__ == '__main__':
     main()
